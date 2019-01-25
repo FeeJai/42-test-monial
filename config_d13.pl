89c0067d6cbd8c707fd83445fa22e8aeea6d477e
Diego Canez <dicanez@e1z1r4p34.42.us.org>
2019-01-24 20:32:37 -0800

    Config file for day 13

---
+++ config_d13.pl
@@ -0,0 +1,241 @@
+ex00 -N -f1=btree_create_node.c -f2=ft_btree.h
+
+lol hi
+main -f=btree_create_node.c ====
+#include "ft_btree.h"
+
+t_btree		*btree_create_node(void *item);
+
+int main()
+{
+	t_btree* node = btree_create_node("asdf");
+	printf("%s\n", (char*)node->item);
+	printf("%p\n", node->left);
+	printf("%p\n", node->right);
+}
+==== check -e ====
+$expected = "asdf\n0x0\n0x0\n";
+====
+
+ex01 -N -f1=btree_apply_prefix.c -f2=ft_btree.h
+
+lol hi
+main -f=btree_apply_prefix.c ====
+#include "ft_btree.h"
+#include <string.h>
+
+void	btree_apply_prefix(t_btree *root, void (applyf)(void *));
+
+t_btree		*bcn(void *item)
+{
+	t_btree *node;
+
+	node = malloc(sizeof(t_btree));
+	node->item = item;
+	node->left = 0;
+	node->right = 0;
+	return (node);
+}
+
+void	b_in(t_btree **root, void *item, int (*cmpf)(void *, void *))
+{
+	t_btree **node_it;
+
+	if(*root)
+	{
+		node_it = root;
+		while (*node_it) {
+			if (cmpf(item, (*node_it)->item) >= 0)
+				node_it = &((*node_it)->right);
+			else
+				node_it = &((*node_it)->left);
+		}
+		*node_it = bcn(item);
+	}
+	else
+	{
+		*root = bcn(item);
+	}
+}
+
+int main()
+{
+	t_btree* root = 0;
+
+	btree_apply_prefix(root, (void(*)(void *))printf);
+	printf("\n");
+	b_in(&root, "6", (int(*)(void *, void *))strcmp);
+	b_in(&root, "4", (int(*)(void *, void *))strcmp);
+	b_in(&root, "7", (int(*)(void *, void *))strcmp);
+	b_in(&root, "3", (int(*)(void *, void *))strcmp);
+	b_in(&root, "5", (int(*)(void *, void *))strcmp);
+
+	btree_apply_prefix(root, (void(*)(void *))printf);
+}
+==== check -e ====
+$expected = "\n64357";
+====
+
+ex02 -N -f1=btree_apply_infix.c -f2=ft_btree.h
+
+lol hi
+main -f=btree_apply_infix.c ====
+#include "ft_btree.h"
+#include <string.h>
+
+void	btree_apply_infix(t_btree *root, void (applyf)(void *));
+
+t_btree		*bcn(void *item)
+{
+	t_btree *node;
+
+	node = malloc(sizeof(t_btree));
+	node->item = item;
+	node->left = 0;
+	node->right = 0;
+	return (node);
+}
+
+void	b_in(t_btree **root, void *item, int (*cmpf)(void *, void *))
+{
+	t_btree **node_it;
+
+	if(*root)
+	{
+		node_it = root;
+		while (*node_it) {
+			if (cmpf(item, (*node_it)->item) >= 0)
+				node_it = &((*node_it)->right);
+			else
+				node_it = &((*node_it)->left);
+		}
+		*node_it = bcn(item);
+	}
+	else
+	{
+		*root = bcn(item);
+	}
+}
+
+int main()
+{
+	t_btree* root = 0;
+
+	btree_apply_infix(root, (void(*)(void *))printf);
+	printf("\n");
+	b_in(&root, "6", (int(*)(void *, void *))strcmp);
+	b_in(&root, "4", (int(*)(void *, void *))strcmp);
+	b_in(&root, "7", (int(*)(void *, void *))strcmp);
+	b_in(&root, "3", (int(*)(void *, void *))strcmp);
+	b_in(&root, "5", (int(*)(void *, void *))strcmp);
+
+	btree_apply_infix(root, (void(*)(void *))printf);
+}
+==== check -e ====
+$expected = "\n34567";
+====
+
+ex03 -N -f1=btree_apply_suffix.c -f2=ft_btree.h
+
+lol hi
+main -f=btree_apply_suffix.c ====
+#include "ft_btree.h"
+#include <string.h>
+
+void	btree_apply_suffix(t_btree *root, void (*applyf)(void *));
+
+t_btree		*bcn(void *item)
+{
+	t_btree *node;
+
+	node = malloc(sizeof(t_btree));
+	node->item = item;
+	node->left = 0;
+	node->right = 0;
+	return (node);
+}
+
+void	b_in(t_btree **root, void *item, int (*cmpf)(void *, void *))
+{
+	t_btree **node_it;
+
+	if(*root)
+	{
+		node_it = root;
+		while (*node_it) {
+			if (cmpf(item, (*node_it)->item) >= 0)
+				node_it = &((*node_it)->right);
+			else
+				node_it = &((*node_it)->left);
+		}
+		*node_it = bcn(item);
+	}
+	else
+	{
+		*root = bcn(item);
+	}
+}
+
+int main()
+{
+	t_btree* root = 0;
+
+	btree_apply_suffix(root, (void(*)(void *))printf);
+	printf("\n");
+	b_in(&root, "6", (int(*)(void *, void *))strcmp);
+	b_in(&root, "4", (int(*)(void *, void *))strcmp);
+	b_in(&root, "7", (int(*)(void *, void *))strcmp);
+	b_in(&root, "3", (int(*)(void *, void *))strcmp);
+	b_in(&root, "5", (int(*)(void *, void *))strcmp);
+
+	btree_apply_suffix(root, (void(*)(void *))printf);
+}
+==== check -e ====
+$expected = "\n35476";
+====
+
+ex04 -N -f1=btree_insert_data.c -f2=ft_btree.h
+
+lol hi
+main -f=btree_insert_data.c ====
+#include "ft_btree.h"
+#include <string.h>
+
+void	btree_insert_data(t_btree **root, void *item, int (*cmpf)(void *, void *));
+
+void	bai(t_btree *root, void (*applyf)(void *))
+{
+	if (root)
+	{
+		bai(root->left, applyf);
+		applyf(root->item);
+		bai(root->right, applyf);
+	}
+}
+
+t_btree		*bcn(void *item)
+{
+	t_btree *node;
+
+	node = malloc(sizeof(t_btree));
+	node->item = item;
+	node->left = 0;
+	node->right = 0;
+	return (node);
+}
+
+int main()
+{
+	t_btree* root = 0;
+
+	btree_insert_data(&root, "6", (int(*)(void *, void *))strcmp);
+	btree_insert_data(&root, "4", (int(*)(void *, void *))strcmp);
+	btree_insert_data(&root, "7", (int(*)(void *, void *))strcmp);
+	btree_insert_data(&root, "3", (int(*)(void *, void *))strcmp);
+	btree_insert_data(&root, "5", (int(*)(void *, void *))strcmp);
+
+	bai(root, (void(*)(void *))printf);
+}
+==== check -e ====
+$expected = "34567";
+====
